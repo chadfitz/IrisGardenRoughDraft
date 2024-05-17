3e2771d02e7b52a4cf1979a1a140753acc86cdfc
chadfitz <fitzchad@gmail.com>
2024-05-17 15:30:43 -0700

    Create top menu layout

--- IrisGardenRoughDraft/ContentView.swift
+++ IrisGardenRoughDraft/ContentView.swift
@@ -9,11 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        // Action bar w/NavigationStack
+//        NavigationStack {
+//            Text("test")
+//                .navigationTitle("Home")
+//                .toolbar {
+//                    ToolbarItem(placement: .topBarLeading) {
+//                        Button(action: {
+//                            // to-do
+//                        }, label: {
+//                            Image(systemName: "line.horizontal.3")
+//                        })
+//                    }
+//                    ToolbarItem(placement: .topBarTrailing) {
+//                        Button(action: {
+//                            // to-do
+//                        }, label: {
+//                            Image(systemName: "gearshape.fill")
+//                        })
+//                    }
+//                }
+//            
+//        }
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Button(action: {
+                    // to-do
+                }, label: {
+                    Image(systemName: "line.horizontal.3")
+                })
+                Text("Home")
+                Spacer()
+                Button(action: {
+                    // to-do
+                }, label: {
+                    Image(systemName: "gearshape.fill")
+                })
+            }
+            .foregroundColor(.purple)
+            Spacer()
         }
         .padding()
     }
